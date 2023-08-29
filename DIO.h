103bd3882e83629ed9c09555198d156341c5fe3a
Mariam Ezzat <85201077+MariamEzzat1@users.noreply.github.com>
2023-08-29 06:23:28 +0300

    Update DIO.h

--- DIO.h
+++ DIO.h
@@ -1,9 +1,3 @@
-/*
- * DIO.h
- *
- *  Created on: Aug 13, 2023
- *      Author: Lenovo
- */
 
 #ifndef DIO_H_
 #define DIO_H_
@@ -12,14 +6,14 @@
 #include "hw_gpio.h"
 #include "std_types.h"
 
-#define Fcpu 16000000
+#define Fcpu                   (uint32)16000000
 
-#define PORTA_ID               0
-#define PORTB_ID               1
-#define PORTC_ID               2
-#define PORTD_ID               3
-#define PORTE_ID               4
-#define PORTF_ID               5
+#define PORTA_ID               (uint8)0
+#define PORTB_ID               (uint8)1
+#define PORTC_ID               (uint8)2
+#define PORTD_ID               (uint8)3
+#define PORTE_ID               (uint8)4
+#define PORTF_ID               (uint8)5
 
 
 typedef enum
@@ -32,12 +26,12 @@ typedef enum
     DISABLE, ENABLE
 }ENABLE_DISABLE;
 
-#define LOGIC_HIGH              1
-#define LOGIC_LOW               0
+#define LOGIC_HIGH              1U
+#define LOGIC_LOW               0U
 
-#define RED_LED 1
-#define BLUE_LED 2
-#define GREEN_LED 3
+#define RED_LED                 1U
+#define BLUE_LED                2U
+#define GREEN_LED               3U
 
 
 void DIO_Init(uint8 port_num);
@@ -46,7 +40,7 @@ void DIO_configurePin(uint8 port_num, uint8 pin_num, GPIO_PinDirectionType direc
 
 void DIO_Write_Pin(uint8 port_num, uint8 pin_num, uint8 value);
 
-//void DIO_setPinDirection(uint8 port_num, uint8 pin_num, GPIO_PinDirectionType direction);
+
 
 uint8 DIO_readPin(uint8 port_num, uint8 pin_num);
 
