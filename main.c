d4046b1f0f18ea540a69110e5697d9d17480dce8
Mariam Ezzat <85201077+MariamEzzat1@users.noreply.github.com>
2023-08-29 06:25:24 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 
 #include "rgb_app.h"
 #include "timer_captureTime.h"
+int main(void);
 int main(void)
 {
-    buttonInit(PORTD_ID, 0);
+    buttonInit(PORTD_ID, 0U);
     LedInit();
     wtimer2_Init();
 
